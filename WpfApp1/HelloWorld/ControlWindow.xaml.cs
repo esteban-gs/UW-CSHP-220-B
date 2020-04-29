b316e92dab4077d945c3ac899aa832c8cb6475eb
esteban-gs <comsat61@gmail.com>
2020-04-28 21:55:53 -0700

    Homework4

---
+++ WpfApp1/HelloWorld/ControlWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HelloWorld
+{
+    /// <summary>
+    /// Interaction logic for ControlWindow.xaml
+    /// </summary>
+    public partial class ControlWindow : Window
+    {
+        public ControlWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void uxLocal_Clicked(object sender, RoutedEventArgs e)
+        {
+            var checkBox = (CheckBox)sender;
+            if (checkBox.IsChecked.Value)
+            {
+                MessageBox.Show($"Local Checked");
+            }
+            else
+            {
+                MessageBox.Show($"Local UnChecked");
+            }
+        }
+
+        private void uxNavigator_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            // Convert the Uri into a string
+            var fileName = e.Uri.AbsoluteUri;
+
+            // Pass the fileName to the helper class
+            var processStartInfo = new ProcessStartInfo(fileName)
+            {
+                UseShellExecute = true,
+                Verb = "open",
+            };
+
+            // Start a new process
+            Process.Start(processStartInfo);
+        }
+    }
+}
---
+++ WpfApp1/Homework4/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Homework4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private bool IsValidPostalCode { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void NumberValidationTextBox(object sender, TextChangedEventArgs e)
+        {
+            var fullText = UxNumberTextBox.Text;
+
+            var usZipRegEx = @"^(\d{5}(?:\-\d{4})?)$";
+            var caZipRegEx = @"^[ABCEGHJKLMNPRSTVXY]{1}\d{1}[A-Z]{1} *\d{1}[A-Z]{1}\d{1}$";
+            Regex usRegex = new Regex(usZipRegEx);
+            Regex caRegex = new Regex(caZipRegEx);
+
+            IsValidPostalCode = (usRegex.IsMatch(fullText)) || (caRegex.IsMatch(fullText));
+
+            //Enable Submit if validation passes
+            UxSubmit.IsEnabled = IsValidPostalCode;
+
+            Uxoutput.Content = $"Valid Postal Code: {IsValidPostalCode}";
+        }
+    }
+}
